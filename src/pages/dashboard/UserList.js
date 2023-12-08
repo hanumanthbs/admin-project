d54ae79466c079e44170d49f7eda12793f7727be
hanumanthbs <74710737+hanumanthbs@users.noreply.github.com>
2023-12-08 18:26:33 +0530

    changes

---
+++ src/pages/dashboard/UserList.js
@@ -0,0 +1,6 @@
+import React from "react";
+import { PageTrafficTable } from "../../components/Tables";
+
+export default () => {
+  return <PageTrafficTable />;
+};
--- src/pages/examples/Signin.js
+++ src/pages/examples/Signin.js
@@ -1,39 +1,88 @@
-
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faAngleLeft, faEnvelope, faUnlockAlt } from "@fortawesome/free-solid-svg-icons";
-import { faFacebookF, faGithub, faTwitter } from "@fortawesome/free-brands-svg-icons";
-import { Col, Row, Form, Card, Button, FormCheck, Container, InputGroup } from '@themesberg/react-bootstrap';
-import { Link } from 'react-router-dom';
-
-import { Routes } from "../../routes";
+import { faEnvelope, faUnlockAlt } from "@fortawesome/free-solid-svg-icons";
+import {
+  Col,
+  Row,
+  Form,
+  Card,
+  Button,
+  FormCheck,
+  Container,
+  InputGroup,
+} from "@themesberg/react-bootstrap";
 import BgImage from "../../assets/img/illustrations/signin.svg";
+import adsyLogo from "../../assets/img/adsy-logo.jpg";
 
+const initialValues = {
+  email: "",
+  password: "",
+};
 
-export default () => {
+function Signin() {
+  const [values, setValues] = useState(initialValues);
+
+  const handleChange = (e) => {
+    const copyValues = { ...values };
+    copyValues[e.target.name] = e.target.value;
+    setValues(copyValues);
+  };
+
+  function authenticateUser(e) {
+    e.preventDefault();
+    const temp = {};
+
+    temp.email = values.email;
+    temp.password = values.password;
+
+    axios
+      .post("https://adsy.co.in/Backend/api/Auth_Admin", temp)
+      .then((res) => {
+        if (res.data.status === 1) {
+          window.location.href = "/#/dashboard/overview";
+        } else {
+          alert(res.data.message);
+        }
+      })
+      .catch((err) => console.error(err));
+  }
+
   return (
     <main>
       <section className="d-flex align-items-center my-5 mt-lg-6 mb-lg-5">
         <Container>
-          <p className="text-center">
-            <Card.Link as={Link} to={Routes.DashboardOverview.path} className="text-gray-700">
-              <FontAwesomeIcon icon={faAngleLeft} className="me-2" /> Back to homepage
-            </Card.Link>
-          </p>
-          <Row className="justify-content-center form-bg-image" style={{ backgroundImage: `url(${BgImage})` }}>
-            <Col xs={12} className="d-flex align-items-center justify-content-center">
+          <Row
+            className="justify-content-center form-bg-image"
+            style={{ backgroundImage: `url(${BgImage})` }}
+          >
+            <Col
+              xs={12}
+              className="d-flex align-items-center justify-content-center"
+            >
               <div className="bg-white shadow-soft border rounded border-light p-4 p-lg-5 w-100 fmxw-500">
-                <div className="text-center text-md-center mb-4 mt-md-0">
-                  <h3 className="mb-0">Sign in to our platform</h3>
+                <div className="text-center text-md-center ">
+                  <h3 className="mb-0">
+                    <img src={adsyLogo} alt="logo" width="120px" />
+                  </h3>
                 </div>
-                <Form className="mt-4">
+
+                <Form className="mt-4" onSubmit={authenticateUser}>
                   <Form.Group id="email" className="mb-4">
-                    <Form.Label>Your Email</Form.Label>
+                    <Form.Label>Email</Form.Label>
                     <InputGroup>
                       <InputGroup.Text>
                         <FontAwesomeIcon icon={faEnvelope} />
                       </InputGroup.Text>
-                      <Form.Control autoFocus required type="email" placeholder="example@company.com" />
+                      <Form.Control
+                        name="email"
+                        value={values.email}
+                        onChange={handleChange}
+                        autoFocus
+                        required
+                        type="email"
+                        placeholder="example@company.com"
+                      />
                     </InputGroup>
                   </Form.Group>
                   <Form.Group>
@@ -43,15 +92,29 @@ export default () => {
                         <InputGroup.Text>
                           <FontAwesomeIcon icon={faUnlockAlt} />
                         </InputGroup.Text>
-                        <Form.Control required type="password" placeholder="Password" />
+                        <Form.Control
+                          name="password"
+                          value={values.password}
+                          onChange={handleChange}
+                          required
+                          type="password"
+                          placeholder="Password"
+                        />
                       </InputGroup>
                     </Form.Group>
                     <div className="d-flex justify-content-between align-items-center mb-4">
                       <Form.Check type="checkbox">
-                        <FormCheck.Input id="defaultCheck5" className="me-2" />
-                        <FormCheck.Label htmlFor="defaultCheck5" className="mb-0">Remember me</FormCheck.Label>
+                        {/* <FormCheck.Input id="defaultCheck5" className="me-2" /> */}
+                        <FormCheck.Label
+                          htmlFor="defaultCheck5"
+                          className="mb-0"
+                        >
+                          {/* Remember me */}
+                        </FormCheck.Label>
                       </Form.Check>
-                      <Card.Link className="small text-end">Lost password?</Card.Link>
+                      <Card.Link className="small text-end">
+                        Forgot password?
+                      </Card.Link>
                     </div>
                   </Form.Group>
                   <Button variant="primary" type="submit" className="w-100">
@@ -59,28 +122,41 @@ export default () => {
                   </Button>
                 </Form>
 
-                <div className="mt-3 mb-4 text-center">
+                {/* <div className="mt-3 mb-4 text-center">
                   <span className="fw-normal">or login with</span>
-                </div>
-                <div className="d-flex justify-content-center my-4">
-                  <Button variant="outline-light" className="btn-icon-only btn-pill text-facebook me-2">
+                </div> */}
+                {/* <div className="d-flex justify-content-center my-4">
+                  <Button
+                    variant="outline-light"
+                    className="btn-icon-only btn-pill text-facebook me-2"
+                  >
                     <FontAwesomeIcon icon={faFacebookF} />
                   </Button>
-                  <Button variant="outline-light" className="btn-icon-only btn-pill text-twitter me-2">
+                  <Button
+                    variant="outline-light"
+                    className="btn-icon-only btn-pill text-twitter me-2"
+                  >
                     <FontAwesomeIcon icon={faTwitter} />
                   </Button>
-                  <Button variant="outline-light" className="btn-icon-only btn-pil text-dark">
+                  <Button
+                    variant="outline-light"
+                    className="btn-icon-only btn-pil text-dark"
+                  >
                     <FontAwesomeIcon icon={faGithub} />
                   </Button>
-                </div>
-                <div className="d-flex justify-content-center align-items-center mt-4">
+                </div> */}
+                {/* <div className="d-flex justify-content-center align-items-center mt-4">
                   <span className="fw-normal">
                     Not registered?
-                    <Card.Link as={Link} to={Routes.Signup.path} className="fw-bold">
+                    <Card.Link
+                      as={Link}
+                      to={Routes.Signup.path}
+                      className="fw-bold"
+                    >
                       {` Create account `}
                     </Card.Link>
                   </span>
-                </div>
+                </div> */}
               </div>
             </Col>
           </Row>
@@ -88,4 +164,6 @@ export default () => {
       </section>
     </main>
   );
-};
+}
+
+export default Signin;
