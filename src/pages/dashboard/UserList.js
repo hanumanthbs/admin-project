8dbae0a3a02e4672f8896aa6ebc39116bb6e4689
hanumanthbs <74710737+hanumanthbs@users.noreply.github.com>
2024-03-29 16:03:04 +0530

    user list

--- src/pages/dashboard/UserList.js
+++ src/pages/dashboard/UserList.js
@@ -1,6 +1,70 @@
-import React from "react";
-import { PageTrafficTable } from "../../components/Tables";
+import React, { useEffect, useState } from "react";
+import axios from "../../services/Api";
+import DataTable from "react-data-table-component";
 
 export default () => {
-  return <PageTrafficTable />;
+  const [userData, setUserData] = useState([]);
+
+  useEffect(() => {
+    getUserData();
+  }, []);
+
+  const getUserData = async () => {
+    await axios
+      .get("/api/UsersList")
+      .then((res) => {
+        setUserData(res.data.data);
+      })
+      .catch((err) => console.error(err));
+  };
+  console.log("userData", userData);
+
+  const customStyles = {
+    headCells: {
+      style: { fontWeight: "bold" },
+    },
+  };
+
+  const columns = [
+    {
+      name: "#",
+      selector: (row, index) => index + 1,
+      sortable: true,
+      width: "60px",
+    },
+    {
+      name: "",
+      button: true,
+      cell: (row) => (
+        <img src={row.pic} alt="Profile Pic" className="user-avatar" />
+      ),
+      sortable: true,
+    },
+    { name: "Name", selector: (row) => row.Name, sortable: true },
+    { name: "Email", selector: (row) => row.Email, sortable: true },
+    { name: "Mobile", selector: (row) => row.Mobile, sortable: true },
+    { name: "Password", selector: (row) => row.Password, sortable: true },
+    { name: "City", selector: (row) => row.City, sortable: true },
+    {
+      name: "Completed Adsy",
+      selector: (row) => row.CompletedAdsy,
+      sortable: true,
+    },
+    {
+      name: "Total Earning",
+      selector: (row) => row.TotalEarning,
+      sortable: true,
+    },
+  ];
+
+  return (
+    <div className="container">
+      <DataTable
+        columns={columns}
+        data={userData}
+        pagination
+        customStyles={customStyles}
+      ></DataTable>
+    </div>
+  );
 };
